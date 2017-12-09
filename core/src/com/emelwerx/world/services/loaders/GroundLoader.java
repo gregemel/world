6e1e27470c944203b474297bb7c69e689a033067
Greg Emel <greg.emel@gmail.com>
2017-12-09 10:13:54 -0800

    moved ground loader code to scene component factory

--- core/src/com/emelwerx/world/services/loaders/GroundLoader.java
+++ core/src/com/emelwerx/world/services/loaders/GroundLoader.java
@@ -4,16 +4,11 @@ package com.emelwerx.world.services.loaders;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g3d.Model;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.physics.bullet.Bullet;
-import com.badlogic.gdx.physics.bullet.collision.btCollisionShape;
-import com.badlogic.gdx.physics.bullet.dynamics.btRigidBody;
 import com.badlogic.gdx.utils.JsonValue;
 import com.emelwerx.world.databags.Scene;
 import com.emelwerx.world.databags.components.ModelComponent;
-import com.emelwerx.world.databags.components.SceneComponent;
-import com.emelwerx.world.databags.systemstates.MotionState;
 import com.emelwerx.world.services.factories.ModelComponentFactory;
+import com.emelwerx.world.services.factories.SceneComponentFactory;
 
 import java.util.Locale;
 
@@ -28,7 +23,7 @@ public class GroundLoader {
 
         Entity groundEntity = new Entity();
         ModelComponent modelComponent = attachModelComponent(groundModelFilename, x, y, z, groundEntity);
-        attachSceneComponent(groundEntity, modelComponent);
+        SceneComponentFactory.create(groundEntity, modelComponent);
         scene.setGround(groundEntity);
     }
 
@@ -37,28 +32,5 @@ public class GroundLoader {
         ModelComponent modelComponent = ModelComponentFactory.create(model, x, y, z);
         groundEntity.add(modelComponent);
         return modelComponent;
-    }
-
-    private static void attachSceneComponent(Entity groundEntity, ModelComponent modelComponent) {
-        SceneComponent sceneComponent = new SceneComponent();
-        btCollisionShape shape = Bullet.obtainStaticNodeShape(modelComponent.getModel().nodes);
-        btRigidBody rigidBody = createStaticRigidBody(sceneComponent, shape);
-        sceneComponent.setBody(rigidBody);
-        sceneComponent.getBody().userData = groundEntity;
-        attachMotionState(modelComponent, sceneComponent, rigidBody);
-        groundEntity.add(sceneComponent);
-    }
-
-    private static void attachMotionState(ModelComponent modelComponent, SceneComponent sceneComponent, btRigidBody rigidBody) {
-        MotionState motionState = new MotionState(modelComponent.getInstance().transform);
-        sceneComponent.setMotionState(motionState);
-        rigidBody.setMotionState(motionState);
-    }
-
-    private static btRigidBody createStaticRigidBody(SceneComponent sceneComponent, btCollisionShape shape) {
-        btRigidBody.btRigidBodyConstructionInfo bodyInfo =
-                new btRigidBody.btRigidBodyConstructionInfo(0, null, shape, Vector3.Zero);
-        sceneComponent.setBodyInfo(bodyInfo);
-        return new btRigidBody(bodyInfo);
     }
 }
